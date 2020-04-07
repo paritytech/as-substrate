32313f065d4fb3b82439880465e7749554d30096
Stefanie Doll <stefie.doll@googlemail.com>
2020-04-07 16:00:00 +0200

    Add crypto host functions

--- packages/as-contracts/assembly/lib/crypto.ts
+++ packages/as-contracts/assembly/lib/crypto.ts
@@ -1,6 +1,9 @@
 /// <reference path="../../../../node_modules/assemblyscript/std/assembly/index.d.ts" />
 
 import {
+  ext_hash_blake2_128,
+  ext_hash_blake2_256,
+  ext_hash_keccak_256,
   ext_hash_sha2_256,
 } from "../env";
 
@@ -12,12 +15,48 @@ import {
 
 export namespace crypto {
   /**
-   * @name hashSha256
+   * @name blake2b128
    * @description
-   * @TODO.
+   * Computes the BLAKE2 128-bit hash on the given input buffer.
    */
 
-  export function hashSha256(value: Uint8Array): Uint8Array {
+  export function blake2b128(value: Uint8Array): Uint8Array {
+    const sha256 = new Uint8Array(32);
+    ext_hash_blake2_128(value.dataStart, value.length, sha256.dataStart)
+    return sha256;
+  }
+
+    /**
+   * @name blake2b256
+   * @description
+   * Computes the BLAKE2 256-bit hash on the given input buffer.
+   */
+
+  export function blake2b256(value: Uint8Array): Uint8Array {
+    const sha256 = new Uint8Array(32);
+    ext_hash_blake2_256(value.dataStart, value.length, sha256.dataStart)
+    return sha256;
+  }
+
+    /**
+   * @name keccack256
+   * @description
+   * Computes the KECCAK 256-bit hash on the given input buffer.
+   */
+
+  export function keccack256(value: Uint8Array): Uint8Array {
+    const sha256 = new Uint8Array(32);
+    ext_hash_keccak_256(value.dataStart, value.length, sha256.dataStart)
+    return sha256;
+  }
+
+    /**
+   * @name sha256
+   * @description
+   * Computes the SHA2 256-bit hash on the given input buffer.
+   */
+
+  export function sha256(value: Uint8Array): Uint8Array {
     const sha256 = new Uint8Array(32);
     ext_hash_sha2_256(value.dataStart, value.length, sha256.dataStart)
     return sha256;
