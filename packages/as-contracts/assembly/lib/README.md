9b4b8b6931e5f4be3e6b1860fb41f04b6487b63b
Stefanie Doll <stefie.doll@googlemail.com>
2020-06-23 13:42:58 +0200

    as-contracts clean up of build scripts

---
+++ packages/as-contracts/assembly/lib/README.md
@@ -0,0 +1,3 @@
+# Library
+
+Collection of AssemblyScript functions that interact directly with the Substrate external host functions exposed in [pallet-contracts](https://github.com/paritytech/substrate/blob/master/frame/contracts/COMPLEXITY.md#externalities).
--- packages/as-contracts/index.js
+++ packages/as-contracts/index.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
-const loader = require("@assemblyscript/loader");
-module.exports = loader.instantiateSync(fs.readFileSync(__dirname + "/build/contract.wasm"), { /* imports */ })
+const loader = require("./node_modules/@assemblyscript/loader");
+module.exports = loader.instantiateSync(fs.readFileSync(__dirname + "/build/as-contracts.wasm"), { /* imports */ })
