b844105bf912d1a4998013907eb9ed498347a2be
Stefanie Doll <stefie.doll@googlemail.com>
2020-06-23 15:38:05 +0200

    optimized build script for contract example

--- projects/contract-incrementer/build.sh
+++ projects/contract-incrementer/build.sh
@@ -4,8 +4,13 @@ set -e
 
 PROJNAME=contract-incrementer
 
-cat build/$PROJNAME.wat | sed "s/(import \"env\" \"memory\" (memory \$0 1))/(import \"env\" \"memory\" (memory \$0 2 16))/" > build/$PROJNAME-fixed.wat
+cat build/$PROJNAME-raw.wat | sed "s/(import \"env\" \"memory\" (memory \$0 1))/(import \"env\" \"memory\" (memory \$0 2 16))/" > build/$PROJNAME-fixed.wat
 
 wat2wasm build/$PROJNAME-fixed.wat -o build/$PROJNAME-fixed.wasm 
 
-wasm-prune --exports call,deploy build/$PROJNAME-fixed.wasm build/$PROJNAME-pruned.wasm
+wasm-prune --exports call,deploy build/$PROJNAME-fixed.wasm build/$PROJNAME.wasm
+
+rm -rf build/$PROJNAME-fixed.wasm
+rm -rf build/$PROJNAME-fixed.wat
+
+wasm2wat build/$PROJNAME.wasm -o build/$PROJNAME.wat 
