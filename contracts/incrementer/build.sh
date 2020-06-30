75a7b73dbc9350106d85c67c7b882c2f669e2f97
Stefanie Doll <stefie.doll@googlemail.com>
2020-06-30 16:43:34 +0200

    Use compiler option --initialMemory --maximumMemory instead of bash work-around

--- contracts/incrementer/build.sh
+++ contracts/incrementer/build.sh
@@ -4,12 +4,8 @@ set -e
 
 PROJNAME=incrementer
 
-cat build/$PROJNAME-raw.wat | sed "s/(import \"env\" \"memory\" (memory \$0 1))/(import \"env\" \"memory\" (memory \$0 2 16))/" > build/$PROJNAME-fixed.wat
+wasm2wat build/$PROJNAME-raw.wasm -o build/$PROJNAME-raw.wat 
 
-wat2wasm build/$PROJNAME-fixed.wat -o build/$PROJNAME-fixed.wasm 
-
-wasm-prune --exports call,deploy build/$PROJNAME-fixed.wasm build/$PROJNAME.wasm
-
-rm -rf build/$PROJNAME-fixed.wasm build/$PROJNAME-fixed.wat
+wasm-prune --exports call,deploy build/$PROJNAME-raw.wasm build/$PROJNAME.wasm
 
 wasm2wat build/$PROJNAME.wasm -o build/$PROJNAME.wat 
--- packages/as-runtime/assembly/lib.ts
+++ packages/as-runtime/assembly/lib.ts
@@ -2,17 +2,5 @@
 
 
 export namespace storage {
-  export function setStorage(key: Uint8Array, value: Uint8Array): void {
-    if(key.length === 32) {
-      const pointer = value ? value!.dataStart : 0;
-      const length = value ? value.length : 0;
-    
-      ext_set_storage(key.dataStart, pointer, length);
-    }
-  }
-  export function clearStorage(key: Uint8Array): void {
-    if(key.length === 32) {
-      ext_clear_storage(key.dataStart, pointer, length);
-    }
-  }
+
 }
